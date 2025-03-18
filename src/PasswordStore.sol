c86160e607bc4fad55b2882ca9f3120729c560b1
peterberekvolgyi <berekvolgyipeter@gmail.com>
2025-03-18 13:12:19 +0100

    audit

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -11,6 +11,7 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit the s_password is not actually private!
     string private s_password;
 
     event SetNewPassword();
@@ -23,6 +24,10 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    // q can a non-owner set a psw?
+    // q should a non-owner set a psw?
+    // @ audit any user can set a psw
+    // missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNewPassword();
@@ -30,6 +35,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     // @audit no param
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
